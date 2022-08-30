mmsqe/tendermint

commit d1da4e1780d8f9030bb474d70219e13c1c4170cd

keep event type for match query

libs/pubsub/query/query.go
@@ -67,13 +67,36 @@ func Compile(ast syntax.Query) (*Query, error) {
 	return &Query{ast: ast, conds: conds}, nil
 }
 
+func expandEvents(flattenedEvents map[string][]string) []types.Event {
+	events := make([]types.Event, len(flattenedEvents))
+
+	for composite, values := range flattenedEvents {
+		tokens := strings.Split(composite, ".")
+
+		attrs := make([]types.EventAttribute, len(values))
+		for i, v := range values {
+			attrs[i] = types.EventAttribute{
+				Key:   []byte(tokens[len(tokens)-1]),
+				Value: []byte(v),
+			}
+		}
+
+		events = append(events, types.Event{
+			Type:       strings.Join(tokens[:len(tokens)-1], "."),
+			Attributes: attrs,
+		})
+	}
+
+	return events
+}
+
 // Matches satisfies part of the pubsub.Query interface.  This implementation
 // never reports an error. A nil *Query matches all events.
-func (q *Query) Matches(events []types.Event) (bool, error) {
+func (q *Query) Matches(events map[string][]string) (bool, error) {
 	if q == nil {
 		return true, nil
 	}
-	return q.matchesEvents(events), nil
+	return q.matchesEvents(expandEvents(events)), nil
 }
 
 // String matches part of the pubsub.Query interface.
@@ -121,9 +144,9 @@ func (c condition) findAttr(event types.Event) ([]string, bool) {
 	}
 	var vals []string
 	for _, attr := range event.Attributes {
-		fullName := event.Type + "." + attr.Key
+		fullName := event.Type + "." + string(attr.Key)
 		if fullName == c.tag {
-			vals = append(vals, attr.Value)
+			vals = append(vals, string(attr.Value))
 		}
 	}
 	return vals, false

commit 0382ec30b5fbe22906613ba3846e9aaf8b56d8d7

avoid empty events
* this bug was introduced in d1da4e1780d8f9030bb474d70219e13c1c4170cd
* add expand and flatten test

libs/pubsub/query/query.go
@@ -68,7 +68,7 @@ func Compile(ast syntax.Query) (*Query, error) {
 }
 
 func ExpandEvents(flattenedEvents map[string][]string) []types.Event {
-	events := make([]types.Event, len(flattenedEvents))
+	events := make([]types.Event, 0)
 
 	for composite, values := range flattenedEvents {
 		tokens := strings.Split(composite, ".")

libs/pubsub/query/query_test.go
@@ -1,11 +1,15 @@
 package query_test
 
 import (
+	"encoding/json"
 	"fmt"
+	"sort"
 	"strings"
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/require"
+	"github.com/tendermint/tendermint/abci/types"
 	"github.com/tendermint/tendermint/libs/pubsub"
 	"github.com/tendermint/tendermint/libs/pubsub/query"
 	"github.com/tendermint/tendermint/libs/pubsub/query/syntax"
@@ -56,6 +60,115 @@ var apiEvents = map[string][]string{
 	},
 	"transfer.amount": {
 		"160",
+	},
+}
+
+var apiTypeEvents = []types.Event{
+	{
+		Type: "tm",
+		Attributes: []types.EventAttribute{
+			{
+				Key:   []byte("event"),
+				Value: []byte("Tx"),
+			},
+		},
+	},
+	{
+		Type: "tm",
+		Attributes: []types.EventAttribute{
+			{
+				Key:   []byte("hash"),
+				Value: []byte("XYZ"),
+			},
+		},
+	},
+	{
+		Type: "tm",
+		Attributes: []types.EventAttribute{
+			{
+				Key:   []byte("height"),
+				Value: []byte("5"),
+			},
+		},
+	},
+	{
+		Type: "rewards.withdraw",
+		Attributes: []types.EventAttribute{
+			{
+				Key:   []byte("address"),
+				Value: []byte("AddrA"),
+			},
+			{
+				Key:   []byte("address"),
+				Value: []byte("AddrB"),
+			},
+		},
+	},
+	{
+		Type: "rewards.withdraw",
+		Attributes: []types.EventAttribute{
+			{
+				Key:   []byte("source"),
+				Value: []byte("SrcX"),
+			},
+			{
+				Key:   []byte("source"),
+				Value: []byte("SrcY"),
+			},
+		},
+	},
+	{
+		Type: "rewards.withdraw",
+		Attributes: []types.EventAttribute{
+			{
+				Key:   []byte("amount"),
+				Value: []byte("100"),
+			},
+			{
+				Key:   []byte("amount"),
+				Value: []byte("45"),
+			},
+		},
+	},
+	{
+		Type: "rewards.withdraw",
+		Attributes: []types.EventAttribute{
+			{
+				Key:   []byte("balance"),
+				Value: []byte("1500"),
+			},
+			{
+				Key:   []byte("balance"),
+				Value: []byte("999"),
+			},
+		},
+	},
+	{
+		Type: "transfer",
+		Attributes: []types.EventAttribute{
+			{
+				Key:   []byte("sender"),
+				Value: []byte("AddrC"),
+			},
+		},
+	},
+	{
+		Type: "transfer",
+		Attributes: []types.EventAttribute{
+			{
+				Key:   []byte("recipient"),
+				Value: []byte("AddrD"),
+			},
+		},
+	},
+	{
+		Type: "transfer",
+		Attributes: []types.EventAttribute{
+			{
+				Key:   []byte("amount"),
+				Value: []byte("160"),
+			},
+		},
 	},
 }
 
@@ -220,6 +333,37 @@ func TestCompiledMatches(t *testing.T) {
 					tc.s, tc.events, got, tc.matches)
 			}
 		})
+	}
+}
+
+func sortEvents(events []types.Event) []types.Event {
+	sort.Slice(events, func(i, j int) bool {
+		if events[i].Type == events[j].Type {
+			return string(events[i].Attributes[0].Key) < string(events[j].Attributes[0].Key)
+		}
+		return events[i].Type < events[j].Type
+	})
+	return events
+}
+
+func TestExpandEvents(t *testing.T) {
+	expanded := query.ExpandEvents(apiEvents)
+	bz, err := json.Marshal(sortEvents(expanded))
+	require.NoError(t, err)
+	bz2, err := json.Marshal(sortEvents(apiTypeEvents))
+	require.NoError(t, err)
+	if string(bz) != string(bz2) {
+		t.Errorf("got %s, want %v", string(bz), string(bz2))
+	}
+}
+
+func TestFlattenEvents(t *testing.T) {
+	bz, err := json.Marshal(query.FlattenEvents(apiTypeEvents))
+	require.NoError(t, err)
+	bz2, err := json.Marshal(apiEvents)
+	require.NoError(t, err)
+	if string(bz) != string(bz2) {
+		t.Errorf("got %s, want %v", string(bz), string(bz2))
 	}
 }
 